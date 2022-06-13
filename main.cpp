2de4b4380430ac9c33a660cdd2aae9d28e0f6d85
nikita <nik.pologov@gmail.com>
2022-06-14 00:13:57 +0300

    refactoring: encapsulation for testing

--- main.cpp
+++ main.cpp
@@ -1,13 +1,8 @@
-#include "LN.h"
 #include "return_codes.h"
-#include <unordered_map>
+#include "solve.h"
 
-#include <fstream>
-#include <functional>
-#include <iostream>
-#include <stack>
-#include <string>
 #include <utility>
+
 
 int main(int argc, char* args[])
 {
@@ -19,7 +14,7 @@ int main(int argc, char* args[])
 	if (!in.is_open())
 	{
 		return ERROR_FILE_NOT_FOUND;
-	};
+	}
 
 	std::ofstream out(args[2]);
 	if (!out.is_open() || !out.good())
@@ -27,129 +22,17 @@ int main(int argc, char* args[])
 		return ERROR_FILE_EXISTS;
 	}
 
-	std::unordered_map< std::string, std::function< LN(LN, LN) > > binary_operations = {
-		{ "+",
-		  [](const LN& x, const LN& y) -> LN
-		  {
-			  return x + y;
-		  } },
-		{ "-",
-		  [](const LN& x, const LN& y) -> LN
-		  {
-			  return x - y;
-		  } },
-		{ "*",
-		  [](const LN& x, const LN& y) -> LN
-		  {
-			  return x * y;
-		  } },
-		{ "/",
-		  [](const LN& x, const LN& y) -> LN
-		  {
-			  return x / y;
-		  } },
-		{ "%",
-		  [](const LN& x, const LN& y) -> LN
-		  {
-			  return x % y;
-		  } },
-	};
-
-	std::unordered_map< std::string, std::function< LN(LN) > > unary_operations = {
-		{ "~",
-		  [](const LN& x) -> LN
-		  {
-			  return ~x;
-		  } },
-		{ "_",
-		  [](const LN& x) -> LN
-		  {
-			  return LN(-1LL) * x;
-		  } },
-	};
-
-	std::unordered_map< std::string, std::function< bool(LN, LN) > > boolean_operations = {
-		{ "<",
-		  [](const LN& x, const LN& y) -> bool
-		  {
-			  return x < y;
-		  } },
-		{ "<=",
-		  [](const LN& x, const LN& y) -> bool
-		  {
-			  return x <= y;
-		  } },
-		{ ">",
-		  [](const LN& x, const LN& y) -> bool
-		  {
-			  return x > y;
-		  } },
-		{ ">=",
-		  [](const LN& x, const LN& y) -> bool
-		  {
-			  return x >= y;
-		  } },
-		{ "==",
-		  [](const LN& x, const LN& y) -> bool
-		  {
-			  return x == y;
-		  } },
-		{ "!=",
-		  [](const LN& x, const LN& y) -> bool
-		  {
-			  return x != y;
-		  } },
-	};
-
-	std::string line;
-	std::stack< LN > data;
-
-	while (in >> line)
+	try
 	{
-		try
+		int ret = solve(in, out);
+		if (ret != ERROR_SUCCESS)
 		{
-			if (binary_operations.count(line) && data.size() >= 2)
-			{
-				LN x = data.top();
-				data.pop();
-				LN y = data.top();
-				data.pop();
-				data.push(binary_operations[line](y, x));
-			}
-			else if (unary_operations.count(line) && !data.empty())
-			{
-				LN x = data.top();
-				data.pop();
-				data.push(unary_operations[line](x));
-			}
-			else if (boolean_operations.count(line) && data.size() >= 2)
-			{
-				LN x = data.top();
-				data.pop();
-				LN y = data.top();
-				data.pop();
-				out << boolean_operations[line](x, y) << '\n';
-			}
-			else if (line != "+" && line != "-" && line != "*" && line != "/" && line != "%" && line != "~" && line != "_" &&
-					 line != "<" && line != "<=" && line != ">" && line != ">=" && line != "==" && line != "!=")
-			{
-				data.push(LN(std::string_view{ line }));
-			}
-		} catch (std::bad_alloc const &)
-		{
-			return ERROR_NOT_ENOUGH_MEMORY;
-		} catch (const char* exception)
-		{
-			return ERROR_NOT_ENOUGH_MEMORY;
-		} catch (std::exception const &)
-		{
-			return ERROR_UNKNOWN;
+			return ret;
 		}
-	}
-	while (!data.empty())
+	} catch (...)
 	{
-		out << data.top();
-		data.pop();
+		return ERROR_UNKNOWN;
 	}
+
 	return ERROR_SUCCESS;
 }
---
+++ solve.h
@@ -0,0 +1,143 @@
+#pragma once
+
+#include "LN.h"
+#include "return_codes.h"
+#include <unordered_map>
+
+#include <fstream>
+#include <functional>
+#include <string>
+#include <utility>
+
+namespace operations
+{
+	std::unordered_map< std::string, std::function< LN(LN, LN) > > binary = {
+		{ "+",
+		  [](const LN& x, const LN& y) -> LN
+		  {
+			  return x + y;
+		  } },
+		{ "-",
+		  [](const LN& x, const LN& y) -> LN
+		  {
+			  return x - y;
+		  } },
+		{ "*",
+		  [](const LN& x, const LN& y) -> LN
+		  {
+			  return x * y;
+		  } },
+		{ "/",
+		  [](const LN& x, const LN& y) -> LN
+		  {
+			  return x / y;
+		  } },
+		{ "%",
+		  [](const LN& x, const LN& y) -> LN
+		  {
+			  return x % y;
+		  } },
+	};
+
+	std::unordered_map< std::string, std::function< LN(LN) > > unary = {
+		{ "~",
+		  [](const LN& x) -> LN
+		  {
+			  return ~x;
+		  } },
+		{ "_",
+		  [](const LN& x) -> LN
+		  {
+			  return LN(-1LL) * x;
+		  } },
+	};
+
+	std::unordered_map< std::string, std::function< bool(LN, LN) > > boolean = {
+		{ "<",
+		  [](const LN& x, const LN& y) -> bool
+		  {
+			  return x < y;
+		  } },
+		{ "<=",
+		  [](const LN& x, const LN& y) -> bool
+		  {
+			  return x <= y;
+		  } },
+		{ ">",
+		  [](const LN& x, const LN& y) -> bool
+		  {
+			  return x > y;
+		  } },
+		{ ">=",
+		  [](const LN& x, const LN& y) -> bool
+		  {
+			  return x >= y;
+		  } },
+		{ "==",
+		  [](const LN& x, const LN& y) -> bool
+		  {
+			  return x == y;
+		  } },
+		{ "!=",
+		  [](const LN& x, const LN& y) -> bool
+		  {
+			  return x != y;
+		  } },
+	};
+
+}	 // namespace operations
+
+int solve(std::istream& in, std::ostream& out)	  // encapsulation for testing
+{
+	std::string line;
+	std::stack< LN > data;
+
+	while (in >> line)
+	{
+		try
+		{
+			if (operations::binary.count(line) && data.size() >= 2)
+			{
+				LN x = data.top();
+				data.pop();
+				LN y = data.top();
+				data.pop();
+				data.push(operations::binary[line](y, x));
+			}
+			else if (operations::unary.count(line) && !data.empty())
+			{
+				LN x = data.top();
+				data.pop();
+				data.push(operations::unary[line](x));
+			}
+			else if (operations::boolean.count(line) && data.size() >= 2)
+			{
+				LN x = data.top();
+				data.pop();
+				LN y = data.top();
+				data.pop();
+				out << operations::boolean[line](y, x) << '\n';
+			}
+			else if (line != "+" && line != "-" && line != "*" && line != "/" && line != "%" && line != "~" && line != "_" &&
+					 line != "<" && line != "<=" && line != ">" && line != ">=" && line != "==" && line != "!=")
+			{
+				data.push(LN(std::string_view{ line }));
+			}
+		} catch (std::bad_alloc const &)
+		{
+			return ERROR_NOT_ENOUGH_MEMORY;
+		} catch (const char* exception)
+		{
+			return ERROR_NOT_ENOUGH_MEMORY;
+		} catch (std::exception const &)
+		{
+			return ERROR_UNKNOWN;
+		}
+	}
+	while (!data.empty())
+	{
+		out << data.top() << '\n';
+		data.pop();
+	}
+	return ERROR_SUCCESS;
+}
