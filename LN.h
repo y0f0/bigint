70d4dab422e19e78a79259c05ce192965162e7cd
nikita <nik.pologov@gmail.com>
2022-06-14 19:53:37 +0300

    refactoring

--- LN.h
+++ LN.h
@@ -9,30 +9,9 @@
 
 class LN
 {
-  private:
-	MyString number;
-	char sign;
-	bool isNan;
-
-	static bool anyIsNaN(const LN& lhs, const LN& rhs) { return lhs.isNan || rhs.isNan; }
-
-	static LN ZERO() { return LN(0LL); }
-	static LN ONE() { return LN(1LL); }
-	static LN MINUS_ONE() { return LN(-1LL); }
-	static LN NaN() { return { MyString("NaN"), '+', true }; }
-
-	static LN abs(const LN& ln) { return ln < 0 ? LN(-1LL) * ln : ln; }
-	static LN abs(LN& ln) { return ln < 0 ? LN(-1LL) * ln : ln; }
-	static void remove_leading_zero(MyString& str);
-	[[nodiscard]] static util::pair< LN, LN > divide(const LN& dividend, const LN& divisor);
-
   public:
-	LN operator++(int);
-	LN& operator--();
-	LN operator--(int);
-
 	LN(MyString number, char sign, bool naNable) :
-		number{ std::move(number) }, sign{ sign }, isNan{ naNable } {}				 // copy constructor
+		number{ std::move(number) }, sign{ sign }, isNan{ naNable } {}				 // constructor
 	LN(const LN& ln) : number{ ln.number }, sign{ ln.sign }, isNan{ ln.isNan } {}	 // copy constructor
 	explicit LN(const long long& ll = 0) :
 		number{ util::to_String(std::abs(ll)) }, sign{ ll < 0 ? '-' : '+' }, isNan{ false }
@@ -72,7 +51,28 @@ class LN
 	explicit operator long long() const;
 	explicit operator bool() const { return *this != ZERO(); }
 
+	LN operator++(int);
+	LN& operator--();
+	LN operator--(int);
+
 	friend std::ostream& operator<<(std::ostream&, const LN&);
+
+  private:
+	MyString number;
+	char sign;
+	bool isNan;
+
+	static bool anyIsNaN(const LN& lhs, const LN& rhs) { return lhs.isNan || rhs.isNan; }
+
+	static LN ZERO() { return LN(0LL); }
+	static LN ONE() { return LN(1LL); }
+	static LN MINUS_ONE() { return LN(-1LL); }
+	static LN NaN() { return { MyString("NaN"), '+', true }; }
+
+	static LN abs(const LN& ln) { return ln < 0 ? LN(-1LL) * ln : ln; }
+	static LN abs(LN& ln) { return ln < 0 ? LN(-1LL) * ln : ln; }
+	static void remove_leading_zero(MyString& str);
+	[[nodiscard]] static util::pair< LN, LN > divide(const LN& dividend, const LN& divisor);
 };
 
 LN operator"" _ln(unsigned long long x);
